1bbddb629fa1aab7bb0118bf6075f141e1f51123
BuildTools <unconfigured@null.spigotmc.org>
2018-06-17 14:49:49 +0300

    starter - comments dont work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
+import { PhotosComponent } from './photos/photos.component';
+import { CommentsComponent } from './comments/comments.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+    },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+    },
+  {
+   path: 'posts',
+    component: PostsComponent
+    },
+  {
+    path: 'photos',
+    component: PhotosComponent
+    },
+    {
+      path: 'comments',
+      component: CommentsComponent
+      },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
